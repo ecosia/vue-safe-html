c1f2861cc62caf2a1220948f0096843c171078b3
Joao Lopes <lopis@users.noreply.github.com>
2021-03-06 16:24:41 +0100

    uncomment tests

--- src/index.test.js
+++ src/index.test.js
@@ -2,43 +2,43 @@ import { createLocalVue, shallowMount } from '@vue/test-utils';
 import Plugin, { allowedTags as defaultAllowedTags } from './index';
 
 describe('Plugin', () => {
-  // describe('Installs', () => {
-  //   const localVue = createLocalVue();
-  //   localVue.directive = jest.fn();
+  describe('Installs', () => {
+    const localVue = createLocalVue();
+    localVue.directive = jest.fn();
 
-  //   beforeEach(() => {
-  //     localVue.directive.mockClear();
-  //   });
+    beforeEach(() => {
+      localVue.directive.mockClear();
+    });
 
-  //   it('Installs directive', () => {
-  //     expect(Plugin).toBeInstanceOf(Object);
-  //     expect(Plugin.install).toBeInstanceOf(Function);
-  //     Plugin.install(localVue);
-  //     expect(localVue.directive).toHaveBeenCalledTimes(1);
-  //     expect(localVue.directive.mock.calls[0][0]).toBe('safe-html');
-  //     expect(localVue.directive.mock.calls[0][1]).toBeInstanceOf(Function);
-  //   });
+    it('Installs directive', () => {
+      expect(Plugin).toBeInstanceOf(Object);
+      expect(Plugin.install).toBeInstanceOf(Function);
+      Plugin.install(localVue);
+      expect(localVue.directive).toHaveBeenCalledTimes(1);
+      expect(localVue.directive.mock.calls[0][0]).toBe('safe-html');
+      expect(localVue.directive.mock.calls[0][1]).toBeInstanceOf(Function);
+    });
 
-  //   it('Installs directive with custom allowed tags', () => {
-  //     const allowedTags = ['a', 'button'];
-  //     Plugin.install(localVue, { allowedTags });
-  //     expect(localVue.directive.mock.calls[0][0]).toBe('safe-html');
-  //     expect(localVue.directive.mock.calls[0][1]).toBeInstanceOf(Function);
-  //   });
-  // });
+    it('Installs directive with custom allowed tags', () => {
+      const allowedTags = ['a', 'button'];
+      Plugin.install(localVue, { allowedTags });
+      expect(localVue.directive.mock.calls[0][0]).toBe('safe-html');
+      expect(localVue.directive.mock.calls[0][1]).toBeInstanceOf(Function);
+    });
+  });
 
   describe('Integration', () => {
-    // it('Sanitizes given string', () => {
-    //   const localVue = createLocalVue();
-    //   localVue.use(Plugin);
-    //   // eslint-disable-next-line vue/one-component-per-file
-    //   const Component = localVue.component('SafeHtmlComponent', {
-    //     template: '<div v-safe-html="\'<p><strong>Safe</strong> HTML<script></script></p>\'"></div>',
-    //   });
-    //   const wrapper = shallowMount(Component, { localVue });
-    //   const expected = '<div><strong>Safe</strong> HTML</div>';
-    //   expect(wrapper.html()).toBe(expected);
-    // });
+    it('Sanitizes given string', () => {
+      const localVue = createLocalVue();
+      localVue.use(Plugin);
+      // eslint-disable-next-line vue/one-component-per-file
+      const Component = localVue.component('SafeHtmlComponent', {
+        template: '<div v-safe-html="\'<p><strong>Safe</strong> HTML<script></script></p>\'"></div>',
+      });
+      const wrapper = shallowMount(Component, { localVue });
+      const expected = '<div><strong>Safe</strong> HTML</div>';
+      expect(wrapper.html()).toBe(expected);
+    });
 
     it('Sanitizes with custom allowed tags', () => {
       const localVue = createLocalVue();
