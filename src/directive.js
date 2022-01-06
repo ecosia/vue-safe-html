34bd991dd0e294e41223867f488c573853147f47
Emanuele <emanuele.marchi@ecosia.org>
2022-01-06 11:59:50 +0100

    Support Vue SSR

--- src/directive.js
+++ src/directive.js
@@ -25,10 +25,18 @@ export default (tags) => {
   const initialTags = areTagsValid(tags) ? tags : defaultTags;
   return (el, binding) => {
     const directiveTags = Object.keys(binding.modifiers);
-    const finalTags = areTagsValid(directiveTags) && directiveTags.length > 0 ?
+    const finalTags = directiveTags.length > 0 && areTagsValid(directiveTags) ?
       directiveTags :
       initialTags;
     const sanitized = sanitizeHTML(binding.value, finalTags);
-    el.innerHTML = sanitized;
+
+    if (typeof el.innerHTML === 'string') {
+      // we're client-side and `el` is an HTMLElement
+      el.innerHTML = sanitized;
+    } else {
+      // we're server-side and `el` is a VNode
+      // see https://vuejs.org/v2/guide/security.html#Injecting-HTML
+      el.data.domProps = { innerHTML: sanitized };
+    }
   };
 };
