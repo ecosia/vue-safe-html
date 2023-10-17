cb3ed1455bae5f42ed3733157aad6fae178e74f7
Emanuele <emanuele.marchi@ecosia.org>
2023-10-17 15:00:27 +0200

    Make webpack config ESM compatible

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,47 @@
-const { resolve } = require('path');
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
 
+const dist = resolve(dirname(fileURLToPath(import.meta.url)), 'dist');
+// eslint-disable-next-line no-underscore-dangle
 const _module = {
-  rules: [
-    {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env'],
-        },
+  rules: [{
+    test: /\.js$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env'],
       },
     },
-  ],
+    resolve: {
+      fullySpecified: false,
+    },
+  }],
 };
 
-module.exports = [
-  {
-    entry: './src/index.js',
-    output: {
-      filename: 'main.common.js',
-      path: resolve(__dirname, 'dist'),
-      library: 'VueSafeHTML',
-      libraryTarget: 'commonjs2',
-      globalObject: '(typeof self !== \'undefined\' ? self : this)',
-    },
-    mode: 'production',
-    module: _module,
+export default [{
+  entry: './src/index.js',
+  output: {
+    filename: 'main.common.js',
+    path: dist,
+    library: 'VueSafeHTML',
+    libraryTarget: 'commonjs2',
+    globalObject: '(typeof self !== \'undefined\' ? self : this)',
   },
-  {
-    entry: './src/index.js',
-    output: {
-      filename: 'main.esm.js',
-      path: resolve(__dirname, 'dist'),
-      libraryTarget: 'module',
-      globalObject: '(typeof self !== \'undefined\' ? self : this)',
-    },
-    mode: 'production',
-    experiments: {
-      outputModule: true, // Enable the "experiments.outputModule" option
-    },
-    module: _module,
+  mode: 'production',
+  module: _module,
+},
+{
+  entry: './src/index.js',
+  output: {
+    filename: 'main.esm.js',
+    path: dist,
+    libraryTarget: 'module',
+    globalObject: '(typeof self !== \'undefined\' ? self : this)',
   },
-];
+  mode: 'production',
+  experiments: {
+    outputModule: true, // Enable the "experiments.outputModule" option
+  },
+  module: _module,
+}];
